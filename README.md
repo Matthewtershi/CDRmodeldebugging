63b5c8bef8db52e395c3f5d7c4814600927fbcb3
Matthewtershi <matthewtershi@gmail.com>
2024-11-16 00:46:06 -0600

    JWT token auth done

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-venv\Scripts\activate
+env\Scripts\activate for backend
+venv\Scripts\activate for model
 
 git init
 git add README.md
@@ -16,5 +17,10 @@ build it to pass parameters idenpendenly for more dyanmic frontend
 
 no need to use pickle or joblib since no need for training
 
+Every time you make a major change to the backend run: python manage.py makemigrations
+
 
 https://www.youtube.com/watch?v=YS55S41x2cA&ab_channel=CodingMSTR
+
+for fetching
+https://www.youtube.com/watch?v=iqlgr1AQt9Q&ab_channel=Pyplane
---
+++ backend/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    """ since django uses ORM format, we can define 
+        the Postgre SQL DB fields with python first 
+        and let django take care of the conversion """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    """ one to many for user to notes
+        foreign key linked user to a single key
+        ondelete param just means when the user is deleted, all associated objects are also deleted """
+
+    def __str__(self):
+        return self.title
+    
+
